0724be7ca60b56fa5cc06e49872f0e58c864b50b
Cristian  CC <cristi.nord@gmail.com>
2018-10-23 16:13:47 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,52 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  name_str =''
+    data.each do |season_key,season_data|
+      if season_key == season
+        season_data.each do |hash_elem|
+          if hash_elem["status"] == "Winner"
+             name_str = hash_elem["name"].split[0]
+          end
+        end
+      end
+    end
+    return name_str
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name_str = ''
+  data.each do |season_key,season_data|
+    season_data.each do |hash_elem|
+      if hash_elem["occupation"] == occupation
+        name_str = hash_elem["name"]
+      end
+    end
+  end
+  return name_str
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  name_str = ''
+  count = 0
+  data.each do |season_key, season_data|
+    season_data.each do |hash_elem|
+      if hash_elem["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  name_str = ''
+  data.each do |season_key,season_data|
+    season_data.each do |hash_elem|
+      if hash_elem["hometown"] == hometown
+            name_str = hash_elem["occupation"]
+      end
+    end
+  end
+  return name_str
 end
 
 def get_average_age_for_season(data, season)
